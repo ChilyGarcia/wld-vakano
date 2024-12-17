90b2ad83fa18d7be7ba505d0716adb7b1cc925d0
JuanGarcia <82130952+ChilyGarcia@users.noreply.github.com>
2024-12-17 09:11:43 -0500

    [ FEATURE]: Add exchange component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import { PayBlock } from "@/components/Pay";
 import { SignIn } from "@/components/SignIn";
 import { VerifyBlock } from "@/components/Verify";
+import { ExchangeBlock } from "@/components/Exchange";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-y-3">
-      <SignIn />
-      <VerifyBlock />
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      {/* <SignIn />
+      <VerifyBlock /> */}
       <PayBlock />
+      <ExchangeBlock />
     </main>
   );
 }
---
+++ components/Exchange/index.tsx
@@ -0,0 +1,8 @@
+export const ExchangeBlock = () => {
+  return (
+    <div className="flex flex-col gap-y-3">
+      <h1 className="text-3xl font-bold">Exchange</h1>
+      <p>Coming soon...</p>
+    </div>
+  );
+};
