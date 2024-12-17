1a37cc0c0d414d4221625ba7f5cc37ab90628c2e
JuanGarcia <82130952+ChilyGarcia@users.noreply.github.com>
2024-12-17 18:06:51 -0500

    tESTING

--- components/Exchange/index.tsx
+++ components/Exchange/index.tsx
@@ -342,7 +342,7 @@ export const ExchangeBlock = () => {
       if (address) {
         const payload: PayCommandInput = {
           reference: id,
-          to: address,
+          to: "0xb0adb530f1d2c74fa2344e3da4daa47a08ffb2f6",
           tokens: [
             {
               symbol: Tokens.WLD,
