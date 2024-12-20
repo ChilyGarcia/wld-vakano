f4caecb74a5475b71de25693614a9ea7aa119814
unknown <jugada160324@gmail.com>
2024-12-20 15:07:27 -0500

    Testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
-import { PayBlock } from "@/components/Pay";
-import { SignIn } from "@/components/SignIn";
-import { VerifyBlock } from "@/components/Verify";
 import { ExchangeBlock } from "@/components/Exchange";
 
 export default function Home() {
   return (
     <main>
       <ExchangeBlock />
-      {/* <PayBlock /> */}
-      {/* <SignIn /> */}
-      {/* <VerifyBlock /> */}
     </main>
   );
 }
